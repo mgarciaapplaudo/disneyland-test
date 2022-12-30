c5703e2de434bf890feed4e5e00a0c758f0704da
Joaquin Mateos <102259797+joaquinbMateos@users.noreply.github.com>
2022-12-30 12:04:04 -0300

    fix: headless mode for github actions

--- tests/visit_website.test.ts
+++ tests/visit_website.test.ts
@@ -2,7 +2,7 @@ import {chromium, test} from "@playwright/test";
 
 test('Open Disney Website', async () =>{
     const browser = await chromium.launch({
-        headless:false
+        headless:true
     })
     const context = await browser.newContext()
     const page = await context.newPage() 
