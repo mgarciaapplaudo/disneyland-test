6357f2a00623a0e9f9a5439243bf3b746b0aebab
joaquinbmateos <joaquin05tw@gmail.com>
2023-01-06 16:44:39 -0300

    fix: Correct config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,7 +41,6 @@ const config: PlaywrightTestConfig = {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-  testMatch: ["**.test.ts"],
   /* Configure projects for major browsers */
   projects: [
     {
