f0b65fca738839277ea03eb3abff6baafa72812c
joaquinbmateos <joaquin05tw@gmail.com>
2023-01-06 17:25:11 -0300

    fix: Attept to fix github actions (again) >:(

--- tests/changeLanguage.test.ts
+++ tests/changeLanguage.test.ts
@@ -1,4 +1,4 @@
-import {test, expect, Browser,firefox,Page,BrowserContext} from "@playwright/test"
+import {test, expect, Browser,firefox, chromium,Page,BrowserContext} from "@playwright/test"
 import Urls from "../utils/urls"
 import HomePage from "../pageObjects/home.page"
 
@@ -11,17 +11,16 @@ let context: BrowserContext
 let page: Page
 
 test.beforeAll(async() =>{
-    browser = await firefox.launch({
+    browser = await chromium.launch({
         headless: true,
     })
     context = await browser.newContext()
     page = await context.newPage()
-    await page.goto(Urls.home)
-    homePage = new HomePage(page)
 })
 
 test("change Language", async() => {
-    await page.waitForLoadState()
+    await page.goto(Urls.home)
+    homePage = new HomePage(page)
     await homePage.clickLanguage()  
     await homePage.clickFirstLanguage()
     await homePage.clickContinue()
